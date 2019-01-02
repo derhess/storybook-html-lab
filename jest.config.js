65e9aab8578752005aaa11bc8cc1d23960cc774f
derhess <FlorianWeil@gmx.de>
2019-01-02 16:48:48 +0100

    Babel Compiler added and some edit on jest config file

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
-const config = require('../../jest.config');
+const config = require("../../jest.config");
 
 module.exports = {
   ...config,
   roots: [__dirname],
   transform: {
     ...config.transform,
-    '.*\\.(html)$': '<rootDir>/node_modules/jest-raw-loader',
+    ".*\\.(html)$": "<rootDir>/node_modules/jest-raw-loader"
   },
-  moduleFileExtensions: [...config.moduleFileExtensions, 'html'],
+  moduleFileExtensions: [...config.moduleFileExtensions, "html"]
 };
